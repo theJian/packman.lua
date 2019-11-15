7839a7e62c5adee39090d6fd5b817eaf2793845c
theJian <thejianmail@gmail.com>
2019-11-15 19:51:04 +0800

    Implement get command

--- packman.lua
+++ packman.lua
@@ -19,17 +19,71 @@ local function init_installation_path()
 	return installation_path
 end
 
-local function define_commands()
-	-- TODO
+local function fetch_plugin(source, dest)
+	local isdir = vim.api.nvim_call_function('isdirectory', {dest})
+	if isdir == 1 then
+		-- TODO: better log message
+		print('plugin is already installed.')
+		return
+	end
+
+	os.execute(string.format('git clone %s %s --recurse-submodules --quiet', source, dest))
 end
+
+local function select_name_from_source(source)
+	local idx = string.find(source, "/[^/]*$")
+	local name = string.sub(source, idx + 1)
+	return name
+end
+
+local function normalize_source(source)
+	if string.match(source, '^https?') or string.match(source, '^git@') then
+		return source
+	end
+
+	if string.match(source, '^.+/.+$') then
+		return 'https://github.com/' .. source
+	end
+
+	error(source .. ' is not a valid plugin source')
+end
+
+---- Public Methods ----
 
 function packman.init()
-	local path = init_installation_path()
-	define_commands()
+	packman.path = init_installation_path()
 end
 
-function packman.get()
-	-- TODO
+function packman.install()
+	
+end
+
+function packman.get(source)
+	if type(source) == 'table' then
+		-- Source is on the first slot if it is a table, install it as a optional plugin.
+		return packman.opt(source[1])
+	end
+	local ok, result = pcall(function() return normalize_source(source) end)
+	if not ok then
+		-- TODO: log error
+		return
+	end
+	source = result
+	local name = select_name_from_source(source)
+	local dest = packman.path .. '/start/' .. name
+	fetch_plugin(source, dest)
+end
+
+function packman.opt(source)
+	local ok, result = pcall(function() return normalize_source(source) end)
+	if not ok then
+		-- TODO: log error
+		return
+	end
+	source = result
+	local name = select_name_from_source(source)
+	local dest = packman.path .. '/opt/' .. name
+	fetch_plugin(source, dest)
 end
 
 function packman.remove()
